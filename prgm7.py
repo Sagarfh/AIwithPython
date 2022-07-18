92596f65477d5a81a565c0be6e5155f286c1e720
Sagar Honnabindagi <37634857+Sagarfh@users.noreply.github.com>
2022-07-18 22:42:59 +0530

    commit 18-07-2022

---
+++ prgm7.py
@@ -0,0 +1,23 @@
+#Dictionaries
+from tokenize import Name
+
+
+Dict = {}
+print("Empty Dictionaries:\n")
+print(Dict)
+
+#Creating Dictionaries with integer key
+Dict = {1:'Geeks',2:'For',3:'Geeks'}
+print("Dictionaries with the use of integer key:\n")
+print(Dict)
+print(Dict[1])
+
+#Creating Dictionaries with mixed key
+Dict = {'Name':'Geeks',1:[1,2,3,4]}
+print("Dictionaries with the use of mixed key:\n")
+print(Dict)
+print(Dict['Name'])
+
+#replace value
+Dict["Name"] = "Sagar"
+print(Dict)
