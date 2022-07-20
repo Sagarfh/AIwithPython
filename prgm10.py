fcf3642a914f6b9fe475e58b3bd7f2e08c4d1f19
Sagar Honnabindagi <37634857+Sagarfh@users.noreply.github.com>
2022-07-20 17:58:11 +0530

    c3

---
+++ prgm10.py
@@ -0,0 +1,9 @@
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+
+print("A is less than B : ",a>b)
+print("A is grater than B : ",a<b)
+print("A is equal than B : ",a==b)
+print("A is not-equal than B : ",a!=b)
+print("A is grater than or equal to B : ",a>=b)
+print("A is less than or equal to B : ",a<=b)
