a71f0a2b6345205592128c9e41594523b92e2f1e
Sagar Honnabindagi <37634857+Sagarfh@users.noreply.github.com>
2022-08-29 18:59:44 +0530

    commit29-08-2022

--- Module2/8.File_Writeing.py
+++ Module2/8.File_Writeing.py
@@ -12,7 +12,6 @@ file1 = open("myfile.txt","r+")
 
 print("Output of Read function is ")
 print(file1.read())
-print()
 
 # seek(n) takes the file handle to the nth
 # bite from the beginning.
@@ -20,14 +19,14 @@ file1.seek(0)
 
 print( "Output of Readline function is ")
 print(file1.readline())
-print()
+
 
 file1.seek(0)
 
 # To show difference between read and readline
 print("Output of Read(9) function is ")
 print(file1.read(9))
-print()
+
 
 file1.seek(0)
 
@@ -38,5 +37,9 @@ file1.seek(0)
 # readlines function
 print("Output of Readlines function is ")
 print(file1.readlines())
-print()
+
+file1.seek(0)
+print("\nOutput of Read single line ")
+print(file1.readlines()[3])
+
 file1.close()
